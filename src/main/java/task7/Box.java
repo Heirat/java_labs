84209976dc27526684be813c286ba856edead5f1
a.chervinskiy <neocron76@gmail.com>
2023-01-11 14:29:27 +0300

    Fix 7 task

--- src/main/java/task7/Box.java
+++ src/main/java/task7/Box.java
@@ -1,50 +1,60 @@
 package task7;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public class Box<E> {
-    private final List<E> array;
+    private final E[] array;
     private final int maxSize;
+    private int size;
+    public int getSize() {
+        return size;
+    }
 
     public Box(int length) {
-        array = new ArrayList<>(length);
+        array = (E[]) new Object[length];
         maxSize = length;
+        size = 0;
     }
 
     public void put(E newElem) {
-        if (size() == maxSize)
+        if (size == maxSize)
             throw new IndexOutOfBoundsException("Коробка заполнена.");
-        array.add(newElem);
+        array[size] = newElem;
+        size += 1;
     }
 
     public E get()  {
-        if (size() == 0)
+        if (size == 0)
             throw new IndexOutOfBoundsException("Коробка пуста.");
-        int randomIndex = (int) (Math.random() * (size() - 1));
-        return array.get(randomIndex);
+        int randomIndex = (int) (Math.random() * (size));
+        return array[randomIndex];
     }
 
     public boolean isEmpty() {
-        return array.isEmpty();
-    }
-
-    public int size() {
-        return array.size();
+        return size == 0;
     }
 
     public boolean remove(E elem) {
-        if (size() == 0)
+        if (size == 0)
             throw new NoSuchElementException("Коробка пуста");
-
-        return array.remove(elem);
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(elem)) {
+                for (int j = i; j < size - 1; j++) {
+                    array[j] = array[j+1];
+                }
+                size--;
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public String toString() {
-        return "Box{" +
-                "array=" + array +
-                '}';
+        StringBuilder result = new StringBuilder("Box ");
+        for (int i = 0; i < size; i++) {
+            result.append(array[i].toString()).append(" ");
+        }
+        return result.toString();
     }
 }
--- src/main/java/task7/Main.java
+++ src/main/java/task7/Main.java
@@ -2,14 +2,17 @@ package task7;
 
 public class Main {
     public static void main(String[] args) {
-        Box<String> box = new Box<>(3);
+        Box<String> box = new Box<>(4);
+
         System.out.println("Коробка пуста? " + box.isEmpty());
         box.put("Карандаш");
         box.put("Линейка");
-        System.out.println("Текущий размер: " + box.size());
+        box.put("Портфель");
+        System.out.println("Текущий размер: " + box.getSize());
         box.put("Пенал");
         System.out.println(box);
-        box.remove("Линейка");
+        System.out.println("Линейка была и удалена? " + box.remove("Линейка"));
+        System.out.println("Бутерброд был и удален? " + box.remove("Бутерброд"));
         System.out.println("После удаления линейки: " + box);
         System.out.println("Случайный элемент: " + box.get());
     }
